e009892cda76276d07ad2db76dce8942372e41aa
jiage110@gmail.com <jiage110@gmail.com@7784a175-1552-6041-ecf7-bb82d0f86263>
2011-11-11 11:09:42 +0000

    消息推送机制

---
+++ trunk/HttpClient/WebRoot/script/SysMSGUtil.js
@@ -0,0 +1,284 @@
+/***
+ * 系统消息发送及弹出js文件
+ * 注意: 只能body中引入,如果在其它地方引入可能会出现错误.
+ * 示例: 需要在消息送达时执行自定的脚本只需要执行如下动作即可, 
+ 	propmtMsgContext.addMsgListener( function (msgs){
+ 		//msgs 即为推送过来的消息数组
+ 	});
+ * 
+ */
+	var d_msg={};
+	var d_msg_head = document.getElementsByTagName("HEAD");
+	//加载jquery文件
+	if( typeof(jQuery)=='undefined'){
+		d_msg.jqueryf = document.createElement('SCRIPT');
+		d_msg.jqueryf.setAttribute("type","text/javascript");
+		d_msg.jqueryf.src = '/itsm/js/jquery/jquery_last.js';
+	}
+
+	//加载弹出框 art组件
+	if (!window.art) {
+			/*d_msg.art = document.createElement('SCRIPT');
+			d_msg.art.src = '/itsm/js/apex/artDialog/artDialog.min.js';
+			d_msg.art.setAttribute("type","text/javascript");
+			//
+			d_msg.skin = document.createElement('SCRIPT');
+			d_msg.skin.setAttribute("type","text/javascript");
+			d_msg.skin.src = '/itsm/js/apex/artDialog/skin.js';*/
+		
+	}
+	
+	//将js文件加载到文档中
+	for(var par in d_msg){
+		if(d_msg_head){
+			d_msg_head[0].appendChild(d_msg[par]); 
+		}else{
+			document.body.appendChild(d_msg[par]);
+		}
+	}
+//--------------------------------------------------分割线 iframe方式 ----------------------------------------------------	
+//服务器推送消息服务JS结构
+function PropmtMsg(){
+	if(this.FunInit){//防止多次构造
+		return ;
+	}
+	this.FunInit = {};
+	this.clientKey = "";//服务器给页面浏览器端定义的key,用来确定一个session打开多个页面的每一个页面
+	this.msgListener=[];//消息监听处理函数列表
+	var that  = this;//将this赋予that变量,方便闭包访问
+	var init =  "0";//初始化标记
+	var iframeObj = {
+		Win : null,
+		frame : null,
+		form : null,
+		iframeHtml : "<form action='/itsm/common/systemMsg.do' method='post' id='msg_form' ><input  name='method' value='getMSG' ><input name = 'clientKey' id='clientKey' ><input id='keepConnect' name='keepConnect' ></form>"
+	};
+	 
+	
+	var bulidIframe = function (){
+		if(!iframeObj.Win){//如果iframeObj.Win在初始化化后不可访问，则存在跨域的问题需要移除此iframe
+			if(iframeObj.frame){
+				jQuery(document.body).remove(iframeObj.frame);
+			}
+			jQuery(document.body).append("<iframe id='_PropmtMsg_Iframe_' name='_PropmtMsg_Iframe_' frameborder='0' src='' heigth='0' width='0'></iframe>");
+		}
+		iframeObj.frame = window.frames['_PropmtMsg_Iframe_'];
+		iframeObj.Win  = iframeObj.frame.window;
+		iframeObj.$ = iframeObj.frame.window.document;
+		//alert(iframeObj.$);
+		iframeObj.$.body.innerHTML =  iframeObj.iframeHtml;
+		iframeObj.form = iframeObj.frame.window.document.getElementById('msg_form');
+	}
+	
+	/**
+	*通过ajax方式 访问服务器
+	*@param keepConnect 此参数是用来通知浏览器端执行探测当前session所打开的所有其它页面是否有效的操作
+	*/
+	var bulidAjaxConnection = function(keepConnect){
+		var kc  = keepConnect?keepConnect :'';
+		jQuery.ajax({
+	      data :{ clientKey: that.clientKey,keepConnect:kc},
+	      dataType : 'JSON',
+	      type : 'post',
+	      timeout:1210000,//20分钟 这个超时配置配置需要配合后台的轮询(周期10分钟)清理超长等待的线程
+	      url : '/itsm/common/systemMsg.do?content=&method=getMSGByAjax',
+	      success : function(jsonStr){
+	      	  if(window.JSON){
+		      	  var data = JSON.parse(jsonStr);
+	      	  }else{
+	      	  	  var data = eval("("+jsonStr+")");
+	      	  }
+		      that.getCallBack()(data);
+	      },
+	      complete : function(XMLHttpRequest, textStatus){
+	      		if(textStatus==='timeout'){
+		      		delete window['propmtMsgContext'];//防止内存泄露,先删除再重新构造请求对象
+		      		propmtMsgContext = new PropmtMsg();
+					if(!propmtMsgContext.getInitStatu()){
+						//外部调用示例,这里的功能是当消息送达后弹出一个消息框(如果需要做别的事情,调用此方法添加一个监听函数)
+						propmtMsgContext.addMsgListener(showMSGBox);
+						propmtMsgContext.setInitStatu();
+						propmtMsgContext.waitMsgFromServer();//等待服务器发送消息
+					}
+	      		}
+	      		
+	      }
+	    })
+	
+	}
+	
+	/**
+	*通过iframe方式访问服务器
+	*@param keepConnect 此参数是用来通知浏览器端执行探测当前session所打开的所有其它页面是否有效的操作
+	*/
+	var bulidIframeConnection = function(keepConnect){
+		bulidIframe();//构造消息获取iframe对象
+		iframeObj.form.clientKey.value= that.clientKey;
+		iframeObj.form.keepConnect.value=keepConnect;
+		iframeObj.form.submit();
+																																							
+	}
+	
+	/**
+	*
+	*@param keepConnect 此参数是用来通知浏览器端执行探测当前session所打开的所有其它页面是否有效的操作
+	*/
+	var getMsg = function(keepConnect){
+		bulidAjaxConnection(keepConnect);//目前还是采用ajax方式进行访问
+		//bulidIframeConnection(keepConnect);//在这里切换 访问方式
+	}
+	
+	//成功获取消息后的回调函数(私有方法)
+	var msgCB  = function msgCallBack(msgs){
+		if(!msgs)return;//发生错误 终止
+		if(msgs.length==0)return;//服务器返回错误,退出
+		//存储从服务器端获取的 客户端编号
+		if(!that.clientKey){
+			that.clientKey = msgs[0].clientKey;
+		}
+		
+		if(!msgs){//消息为空重新获取消息
+			getMsg("keepConnect");
+			return;
+		}
+		
+		var infors  = [];//最终有效的消息列表
+		//当该sessionId的浏览器打开多个页面时,服务器会通知该session所打开的所有页面(此机制为了解决服务端及时发现已经关闭或离开的页面,防止内存泄露),
+		//通知客户端此条连接等待作废(以disable为标记)重新发起消息请求,并为客户端设置clientKey
+		for(var i =0 ;i<msgs.length;i++){
+			var msg  = msgs[i]
+			if(msg.disable){
+				getMsg("keepConnect");  //通知服务器当前客户端页面仍然有效
+				if(msg.clientKey){
+					that.clientKey = msg.clientKey;//重新设置客户端id
+				}
+				return;						
+			}else{
+				delete msg['clientKey'];//不对外公开clientKey
+				infors.push(msg);
+			}
+		}
+		
+		//遍历执行消息送达监听事件
+		for(var i =0 ;i<that.msgListener.length;i++){
+			that.msgListener[i](infors);
+		}
+		getMsg("keepConnect");
+	}
+	
+	
+	
+	
+	
+	
+	/**
+	*启动等待服务器消息的送达(对外公开)
+	*/
+	this.waitMsgFromServer = function(){
+		getMsg();
+	}
+	
+	//对外公开的回调函数get方法
+	this.getCallBack = function (){
+		//返回成功获取消息后的回调函数(私有方法)
+		return msgCB;
+	}
+	
+	this.getInitStatu = function (){
+		return  init==="1";
+	}
+	
+	this.setInitStatu = function (){
+		init = "1";
+	}
+}
+
+
+
+
+
+
+/***
+*添加消息到达自定义监听事件
+*当浏览获取到服务器端推送的消息时,将消息列表对象(消息记录数组)作为参数遍历调用这里添加的函数(回调作用)
+*@param fun 函数对象
+*/
+PropmtMsg.prototype.addMsgListener = function(fun){
+	if( typeof(fun) ==='function'){
+		this.msgListener.push(fun);
+	}
+}
+
+/**
+*为指定用户发送一条消息
+*@param msg 消息内容
+*@param userId 用户id
+*/
+PropmtMsg.prototype.SendMsg = function(msg,userId){
+	if(!msg)return;
+	if(!userId)return;
+	if(msg==''|| userId=='')return;
+	jQuery.ajax({
+      data :{ receiverId: userId, msgstr : msg},
+      dataType : 'html',
+      type : 'post',
+      url : '/itsm/common/systemMsg.do?content=&method=sendMSG'
+    });
+}
+
+
+
+/**
+*弹出消息框
+*@param msgs  msg的数组
+*/
+function showMSGBox(msgs){
+	if(showMSGBox.showPanel){
+			showMSGBox.showPanel.close();
+	}
+	if(msgs.length>0){
+		var msg  = msgs[0];
+	}else{
+		return;
+	}
+	var title  = msg.title;
+	if(!title){
+		title="您有新的消息";
+	}
+	var contenMemo  = msg.memo;
+	if(window.art){
+		if(msg.url) contenMemo+="  <a href ='"+msg.url+"'>更多...</a>";
+		try{
+		//弹出消息框 ,这里只弹出了第一条消息,可以将返回的多条消息,另外展示^_^
+	    	showMSGBox.showPanel = window.art.dialog({
+			    title:title,
+			    left: 'right' ,
+			    top: 'bottom',
+			    width:250,
+			    effect:true,
+			    time:15,
+			    content: contenMemo,
+			    closeFn:function(){
+			    	delete showMSGBox['showPanel'];
+			    }
+			});
+		}catch(e){
+		
+		}
+	}
+}
+
+
+//初始化对象;
+var propmtMsgContext = new PropmtMsg();
+(function(){
+	if(!propmtMsgContext.getInitStatu()){
+		//外部调用示例,这里的功能是当消息送达后弹出一个消息框(如果需要做别的事情,调用此方法添加一个监听函数)
+		propmtMsgContext.addMsgListener(showMSGBox);
+		propmtMsgContext.setInitStatu();
+		propmtMsgContext.waitMsgFromServer();//等待服务器发送消息
+	}
+})();
+
+
+
---
+++ trunk/HttpClient/serverpush/com/wjdeng/web/MSGServlet.java
@@ -0,0 +1,181 @@
+/******************************************************************************** 
+ * Create Author   : Administrator
+ * Create Date     : Sep 3, 2010
+ * File Name       : PaserCtroLServlet.java
+ *
+ ********************************************************************************/
+package com.wjdeng.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import com.wjdeng.SystemMessgeServices;
+import com.wjdeng.client.model.api.AppContext;
+import com.wjdeng.client.model.ctronl.AppStatus;
+import com.wjdeng.client.model.ctronl.ContinueRunCommand;
+import com.wjdeng.client.model.ctronl.DefaultAppContext;
+import com.wjdeng.client.model.ctronl.ModeParament;
+import com.wjdeng.client.model.ctronl.PuaseCommand;
+import com.wjdeng.client.model.ctronl.event.Event;
+import com.wjdeng.client.model.ctronl.event.Listener;
+import com.wjdeng.client.util.LogUtil;
+import com.wjdeng.client.util.StringUtils;
+import com.wjdeng.client.util.SysUtils;
+import com.wjdeng.imp.ExcelUtils;
+import com.wjdeng.model.SystemMsgDataEntity;
+import com.wjdeng.model.User;
+
+public class MSGServlet extends HttpServlet {
+	
+	private SystemMessgeServices systemMessgeServices;
+	
+	/**
+	 */
+	public MSGServlet() {
+		super();
+	}
+
+	/**
+	 */
+	public void destroy() {
+		super.destroy();
+	}
+
+	/**
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+
+	/**
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	public void doPost(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
+			IOException {
+	}
+	
+	
+	/**
+	 * 
+	 * 获取消息记录通过iframe方式
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public void getMSG(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		User user = (User) request.getSession().getAttribute("");
+		String sessionId =request.getSession().getId();
+		String clientKey = SysUtils.trim2null(request.getParameter("clientKey"));
+		String keepConnect = SysUtils.trim2null(request.getParameter("keepConnect"));
+		List<SystemMsgDataEntity> msglist= systemMessgeServices.propmtMSG(user, sessionId,clientKey,keepConnect);
+		String str=JSONArray.fromObject(msglist).toString();
+		request.setAttribute("msgs", str);
+		//response.getWriter().write(str);
+		//this.getServletContext().get
+		//request.
+		//response.
+		//this.
+		request.getRequestDispatcher("/WEB-INF/jsps/common/sysMsgCallBackScript.jsp").include(request, response);
+		//return new ActionForward("/WEB-INF/jsps/common/sysMsgCallBackScript.jsp");
+	}
+	
+	
+	/**
+	 * 
+	 * 获取消息记录通过ajax方式
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	public void getMSGByAjax(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		User user = (User) request.getSession().getAttribute("");
+		String sessionId =request.getSession().getId();
+		String clientKey = SysUtils.trim2null(request.getParameter("clientKey"));
+		String keepConnect = SysUtils.trim2null(request.getParameter("keepConnect"));
+		List<SystemMsgDataEntity> msglist= systemMessgeServices.propmtMSG(user, sessionId,clientKey,keepConnect);
+		String str=JSONArray.fromObject(msglist).toString();
+		response.getWriter().write(str);
+	}
+	
+	
+	/**
+	 * 
+	 * 浏览器端给指定用户发送一条消息
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return 
+	 * @throws Exception
+	 */
+	public void sendMSG( HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		User user = (User) request.getSession().getAttribute("");
+		String receiverId = SysUtils.trim2null(request.getParameter("receiverId"));
+		String msgstr = SysUtils.trim2null(request.getParameter("msgstr"));
+		if(receiverId==null)return ;
+		User rec = new User();
+		rec.setId(Long.valueOf(receiverId));
+		SystemMsgDataEntity msg  = new SystemMsgDataEntity();
+		msg.setCreateUser(user);
+		msg.setReceiver(rec);
+		msg.setMemo(msgstr);
+		msg.setCreateTime(System.currentTimeMillis());
+		systemMessgeServices.sendMsg(msg);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 * 
+	 * @throws ServletException
+	 *             if an error occurs
+	 */
+	public void init() throws ServletException {
+	}
+
+}
